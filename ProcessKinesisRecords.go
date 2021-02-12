48831e621daeb23f672fcd388377fe0eba3ab674
Jo Ann S de Leon <jdeleon@newrelic.com>
2021-02-12 12:15:32 -0500

    initial commit

---
+++ ProcessKinesisRecords.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, kinesisEvent events.KinesisEvent) {
+	for _, record := range kinesisEvent.Records {
+		kinesisRecord := record.Kinesis
+		dataBytes := kinesisRecord.Data
+		dataText := string(dataBytes)
+
+		fmt.Printf("%s Data = %s \n", record.EventName, dataText)
+	}
+}
+
+func main() {
+	lambda.Start(handler)
+}
