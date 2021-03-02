f75be658d83a3645ca84fed725a56d589ef26f3d
Jo Ann S de Leon <jdeleon@newrelic.com>
2021-03-02 15:18:40 -0500

    added NR instrumentation

--- ProcessKinesisRecords.go
+++ ProcessKinesisRecords.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/newrelic/go-agent/v3/integrations/nrlambda"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func handler(ctx context.Context, kinesisEvent events.KinesisEvent) {
@@ -14,10 +15,26 @@ func handler(ctx context.Context, kinesisEvent events.KinesisEvent) {
 		dataBytes := kinesisRecord.Data
 		dataText := string(dataBytes)
 
+		if txn := newrelic.FromContext(ctx); nil != txn {
+			txn.Application().RecordCustomEvent("MyEvent", map[string]interface{}{
+				"zip": "zap",
+			})
+
+			// This attribute gets added to the normal AwsLambdaInvocation event
+			txn.AddAttribute("myCustomData", dataText)
+		}
+
 		fmt.Printf("%s Data = %s \n", record.EventName, dataText)
 	}
 }
 
 func main() {
-	lambda.Start(handler)
+	// Here we are in cold start. Anything you do in main happens once.
+	// In main, we initialize the agent.
+	app, err := newrelic.NewApplication(nrlambda.ConfigOption())
+	if nil != err {
+		fmt.Println("error creating app (invalid config):", err)
+	}
+	// Then we start the lambda handler using `nrlambda` rather than `lambda`
+	nrlambda.Start(handler, app)
 }
