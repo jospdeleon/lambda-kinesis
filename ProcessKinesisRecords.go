9b9f50966348e45f29e2cc0d2d16379af5d0412b
Jo Ann S de Leon <jdeleon@newrelic.com>
2021-04-28 13:26:47 -0400

    correct DT parsing

--- ProcessKinesisRecords.go
+++ ProcessKinesisRecords.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/newrelic/go-agent/v3/integrations/nrlambda"
@@ -14,6 +15,9 @@ import (
 type payload struct {
 	Message string `json:"message"`
 	NrDt    string `json:"nrDt"`
+}
+type nrpayload struct {
+	Newrelic string `json:"newrelic"`
 }
 
 func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
@@ -35,14 +39,24 @@ func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
 				return err
 			}
 
+			//after unmarshalling into p...
+			var np nrpayload
+			err = json.Unmarshal([]byte(p.NrDt), &np)
+			if err != nil {
+				fmt.Printf("json unmarshal error: %v", err)
+				return err
+			}
+
 			hdrs := http.Header{}
-			hdrs.Set(newrelic.DistributedTraceNewRelicHeader, p.NrDt)
+			// hdrs.Set(newrelic.DistributedTraceNewRelicHeader, p.NrDt)
+			hdrs.Set(newrelic.DistributedTraceNewRelicHeader, np.Newrelic)
 
 			// txn.SetWebRequest(newrelic.WebRequest{
 			// 	Header:    hdrs,
 			// 	Transport: newrelic.TransportOther,
 			// })
 
+			fmt.Printf("HDRS value = %s \n", hdrs)
 			txn.AcceptDistributedTraceHeaders(newrelic.TransportOther, hdrs)
 
 			fmt.Printf("Trace Metadata AFTER = %s %s \n", txn.GetTraceMetadata().TraceID, txn.GetTraceMetadata().SpanID)
@@ -68,7 +82,7 @@ func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
 func main() {
 	// Here we are in cold start. Anything you do in main happens once.
 	// In main, we initialize the agent.
-	app, err := newrelic.NewApplication(nrlambda.ConfigOption())
+	app, err := newrelic.NewApplication(nrlambda.ConfigOption(), newrelic.ConfigDebugLogger(os.Stdout))
 	if nil != err {
 		fmt.Println("error creating app (invalid config):", err)
 	}
